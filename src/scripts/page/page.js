3115ac20b9095f382474048bbfafc621a9558700
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-04-19 12:34:54 +0700

    Fix image sizing

--- src/scripts/page/page.js
+++ src/scripts/page/page.js
@@ -3,7 +3,7 @@ import CONFIG from "../global/CONFIG"
 const PAGE = {
   index: `
   <div class="bg-transparent w-screen h-screen flex place-content-center place-items-center">
-    <div class="flex-col max-w-xs sm:max-w-sm md:max-w-md lg:max-w-md xl:max-w-md text-center">
+    <div class="flex-col max-w-xs sm:max-w-sm md:max-w-sm lg:max-w-sm xl:max-w-sm text-center">
       <div id="${CONFIG.GENERATOR_IMAGE_CONTAINER_ID}" class="w-full border-collapse border-2 border-sky-300 text-center mb-4">💕</div>
       <button id="${CONFIG.BUTTON_ID}" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">:v</button>
     </div>
