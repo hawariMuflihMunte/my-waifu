064a106897b90b4210a37bad6a3bb64dac3c18f9
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-04-19 10:48:30 +0700

    Update UI + Minimize image width with its container/wrapper

--- src/scripts/page/page.js
+++ src/scripts/page/page.js
@@ -3,8 +3,8 @@ import CONFIG from "../global/CONFIG"
 const PAGE = {
   index: `
   <div class="bg-transparent w-screen h-screen flex place-content-center place-items-center">
-    <div class="flex-col">
-      <div id="${CONFIG.GENERATOR_IMAGE_CONTAINER_ID}" class="max-w-xs sm:max-w-lg md:max-w-2xl"></div>
+    <div class="flex-col max-w-xs sm:max-w-lg md:max-w-2xl lg:max-w-2xl text-center">
+      <div id="${CONFIG.GENERATOR_IMAGE_CONTAINER_ID}" class="max-w-xs sm:max-w-lg md:max-w-2xl lg:max-w-2xl border-collapse border-2 border-sky-300 text-center mb-4">💕</div>
       <button id="${CONFIG.BUTTON_ID}" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">:v</button>
     </div>
   </div>
